82b419ffd71724a901259f5388f77d6ff0d91b34
Jon Rosenbaum <jrosenbaum@mathematica-mpr.com>
2023-09-20 09:49:30 -0400

    Added mutation observer to ensure account detail menu exists before applying header rule.

--- extension.js
+++ extension.js
@@ -3,10 +3,10 @@
  * Created by fumiharu kinoshita
  */
 const agent = window.navigator.userAgent.toLowerCase();
-const SETTING_KEY = 'awsconsole';
+const SETTING_KEY = "awsconsole";
 
 var intervalId;
-var retryLimit = 60
+var retryLimit = 60;
 
 function getStorage() {
   if (agent.indexOf("chrome") != -1) {
@@ -18,7 +18,7 @@ function getStorage() {
         } else {
           resolve(ruleList);
         }
-      })
+      });
     });
   } else {
     // other browser
@@ -26,68 +26,94 @@ function getStorage() {
   }
 }
 
-
 function applyRule(rule) {
   // header
-  $('header > nav').css('background-color', rule.color);
-  $('.globalNav-0366 > a').css('background-color', rule.color);
+  $("header > nav").css("background-color", rule.color);
+  $(".globalNav-0366 > a").css("background-color", rule.color);
 
   // footer
-  $('#console-nav-footer-inner').css('background-color', rule.color);
+  $("#console-nav-footer-inner").css("background-color", rule.color);
 
   // label
   if (rule.showLabel && rule.label != null && rule.label.length > 0) {
-    $('body').prepend('<span id="ruleLabel">' + rule.label + '</span>');
+    $("body").prepend('<span id="ruleLabel">' + rule.label + "</span>");
   }
 }
 
-
 function findAccount() {
-  var name = $("span[data-testid='awsc-nav-account-menu-button'] span:first").text();
-  var acct = $("div[data-testid='account-detail-menu'] span:nth-child(2)").text();
-  if (name != '') {
-    //console.log(name, acct);
-    acct = acct.replaceAll('-', '');
+  var name = $(
+    "span[data-testid='awsc-nav-account-menu-button'] span:first"
+  ).text();
+  if (name != "") {
+    // console.log(name, acct);
     clearInterval(intervalId);
 
-    var regions = location.search.match(/region=(.*?)(&|$)/);
-    var region = "";
-    if (regions != null && regions.length > 1) {
-      region = regions[1];
-    }
-  
-    // load setting.
-    getStorage().then(ruleList => {
+    // Need to wait for element to be rendered to account for possible racing condition
+    // that would prevent header from showing.
+    waitForElementToExist(
+      "div[data-testid='account-detail-menu'] span:nth-child(2)"
+    ).then((element) => {
+      var acct = element.innerText.replaceAll("-", "");
 
-      ruleList[SETTING_KEY].some(rule => { 
-        var re = new RegExp(rule.user);
-        if (rule.enableRule && (re.test(name) || re.test(acct))) {
-          if (region == rule.region || "all-region" == rule.region) {
-            // apply rule.
-            applyRule(rule);
-            return true;
-          }
+      var regions = location.search.match(/region=(.*?)(&|$)/);
+      var region = "";
+      if (regions != null && regions.length > 1) {
+        region = regions[1];
+      }
+
+      // load setting.
+      getStorage().then(
+        (ruleList) => {
+          ruleList[SETTING_KEY].some((rule) => {
+            var re = new RegExp(rule.user);
+            if (rule.enableRule && (re.test(name) || re.test(acct))) {
+              if (region == rule.region || "all-region" == rule.region) {
+                // apply rule.
+                applyRule(rule);
+                return true;
+              }
+            }
+          });
+        },
+        (err) => {
+          console.error(err);
         }
-      });
-    }, 
-    err => {
-      console.error(err);
+      );
     });
   }
   if (--retryLimit <= 0) {
     clearInterval(intervalId);
   }
+}
 
+function waitForElementToExist(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return document.querySelector(selector);
+    }
+
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      subtree: true,
+      childList: true,
+    });
+  });
 }
 
 // main function
-$(function() {
+$(function () {
   // show/hide label
-  $('#consoleNavHeader').hover( 
-    () => $('#ruleLabel').css('visibility', 'hidden'),
-    () => $('#ruleLabel').css('visibility', '')
+  $("#consoleNavHeader").hover(
+    () => $("#ruleLabel").css("visibility", "hidden"),
+    () => $("#ruleLabel").css("visibility", "")
   );
-  
+
   // waiting loading.
   intervalId = setInterval(findAccount, 1000);
 });
